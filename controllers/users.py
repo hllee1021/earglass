5591da32f0ee52d12cd45e41138aef3fbc144f98
Earglass <earglass@gmail.com>
2020-11-04 18:46:34 +0900

    [feat] implement login/logout/mypage

--- controllers/users.py
+++ controllers/users.py
@@ -4,50 +4,39 @@ from services import users
 controller = Blueprint("users", __name__)
 
 
-@controller.route("/login", methods=["GET"])
-def get_login_page():
-    return render_template("login.html")
-
-
 @controller.route("/login", methods=["POST"])
 def post_login_data():
-    username = request.form.get("username")
+    user_id = request.form.get("username")
     password = request.form.get("password")
-    print(username)
+    print(user_id)
     print(password)
 
     # 로그인 성공
-    if users.verify_user(username, password):
+    if users.verify_user(user_id, password):
         # not "/my", "my". "my" == "/users/my", "/my" = "/my"
         res = make_response(redirect("my"))
-        res.set_cookie("user", username)
-        flash("성공적으로 로그인되었습니다")
+        res.set_cookie("user_id", user_id)
         return res
-
-    # 로그인 실패
     else:
-        flash("로그인에 실패하였습니다")
-        return redirect("my")
-
-
-@controller.route("/my", methods=["GET"])
-def get_my_page():
-    username = request.cookies.get("user")
-    user = users.get_user_by_id(username)
-    if user:
-        return render_template("my.html", user=user)
-    else:
-        return redirect("login")
+        flash("로그인 실패. 다시 시도하세요")
+        return redirect("/")
 
 
 @controller.route("/logout", methods=["GET"])
 def logout():
-    res = make_response(redirect("login"))
+    res = make_response(redirect("/"))
     res.delete_cookie("user")
     return res
 
 
-@controller.route("/test", methods=["GET"])
-def test():
-    return render_template("sign_up.html")
-
+@controller.route("/my", methods=['GET'])
+def mypage():
+    # 쿠키가 있다 -> 로그인된 유저라면
+    user_id = request.cookies.get("user_id")
+    print('user_id:', user_id)
+    user = users.get_user_by_id(user_id)
+    if user:  # 로그인 된 경우
+        return render_template("my.html", user=user)
+    else:
+        flash("로그인되지 않았습니다")
+        return redirect("/")
