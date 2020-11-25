075124067b4f6281cbc9c504e752b5d557724459
Sebastian Jeong <sebastianrcnt@gmail.com>
2020-11-25 17:52:52 +0000

    [fix] add for merge

---
+++ query/Trigger/create_View_EstimatorsInDoneTask.sql
@@ -0,0 +1,9 @@
+-- estimators in done task
+
+CREATE VIEW EstimatorsInDoneTask AS
+    SELECT ROW_NUMBER() OVER() AS INDEX, T.TaskName, DISTINCT E.FK_idEstimator
+    FROM TASK AS T, EVALUATION AS E, PARSING_DSF AS P
+    WHERE T.Status = 'done'
+    AND T.TaskName = P.TaskName
+    AND P.idPARSING_DSF = E.FK_idPARSING_DSF;
+
--- query/Trigger/create_View_SubmittersInDoneTask.sql
+++ query/Trigger/create_View_SubmittersInDoneTask.sql
@@ -1,7 +1,7 @@
 -- submitters in done task
 
 CREATE VIEW SubmittersInDoneTask AS
-    SELECT ROW_NUMBER() OVER AS INDEX, T.TaskName, P.SubmitterID
+    SELECT ROW_NUMBER() OVER() AS INDEX, T.TaskName, DISTINCT P.SubmitterID
     FROM TASK AS T, PARSING_DSF AS P
     WHERE T.Status = 'done'
     AND T.TaskName = P.TaskName
