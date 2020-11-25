a1a2d0532f9ad487362896be56a1de87ec5a9a55
hllee1021 <35593748+hllee1021@users.noreply.github.com>
2020-11-26 01:16:49 +0900

    [feat] estimator router done~~~~~~~~~~~~~

--- controllers/estimator.py
+++ controllers/estimator.py
@@ -16,13 +16,19 @@ def get_estimator_home():
 
 @controller.route("/pdsf_detail", methods=["GET"])
 def get_pdsf_detail():
-    tasks = [{"id": 1, "name":"aa","submitter_name":"hllee1021", "score":"100", "pass":"P","date":"2020/11/20"},{"id": 2, "name":"bbb","submitter_name":"1231asf", "score":"50", "pass":"N","date":"2020/12/20"},{"id": 3, "name":"lala","submitter_name":"qwe123123", "score":"70", "pass":"P","date":"2020/9/20"}]
+    user_index = int(request.cookies.get("user_index"))
+    tasks = services.estimator.evaluated_list(user_index)
+    # tasks = [{"id": 1, "name":"aa","submitter_name":"hllee1021", "score":"100", "pass":"P","date":"2020/11/20"},{"id": 2, "name":"bbb","submitter_name":"1231asf", "score":"50", "pass":"N","date":"2020/12/20"},{"id": 3, "name":"lala","submitter_name":"qwe123123", "score":"70", "pass":"P","date":"2020/9/20"}]
     return render_template("estimator/pdsf_detail.html",tasks=tasks)
 
 #made by 학림
 @controller.route("/estimator_task_info", methods=["GET"])
 def get_estimator_task_info():
-    tasks = {"name":"im_task","description":"card data please~~ samsung good good", "schema_info":"스키마 인포 어떻게 쓰지이", "deadline":"2020/11/20","pass_cut":"중복 튜플 수에 대해서 널널하게 해도 되니까, null 비율에 엄격하게 해주세용"}
+    # task_name=request.cookies.get("task_name")
+    task_name = request.args.get('task_name', 0)
+    tasks=services.estimator.task_detail(task_name)
+    print(tasks)
+    # tasks = {"name":"im_task","description":"card data please~~ samsung good good", "schema_info":"스키마 인포 어떻게 쓰지이", "deadline":"2020/11/20","pass_cut":"중복 튜플 수에 대해서 널널하게 해도 되니까, null 비율에 엄격하게 해주세용"}
     return render_template("/estimator/estimator_task_info.html",tasks=tasks)
 
 #made_by 학림 evaluator_home에서 평가했을때 form action 라우터
@@ -46,16 +52,16 @@ def evaluate():
 @controller.route("/task/download")
 def csv_file_download_with_stream():
     """승수형이 submitter download한거 복사함 일단"""
-    odsf_type_id = int(request.args.get('odsf_type_id', 0))
-    if odsf_type_id != 0:
-        odsf_type = services.submitter.odsf_type_schema_info(odsf_type_id)
+    idPARSING_DSF = int(request.args.get('idPARSING_DSF', 0))
+    if idPARSING_DSF != 0:
+        pdsf_type = services.estimator.pdsf_file_info(idPARSING_DSF)
     else:
         return redirect("/my_task")
 
-    filename = f"{odsf_type['TaskName']}_{odsf_type['DataTypeName']}"
+    filename = f"{pdsf_type['TaskName']}_{pdsf_type['ParsingFile']}"
 
     # schema에 맞는 df 생성
-    schema = json.loads(odsf_type['MappingInfo'])
+    schema = json.loads(pdsf_type['MappingInfo'])
     schema = list(schema.keys())
     temp_df = pd.DataFrame(columns=schema)
 
