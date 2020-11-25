888868323f28fa6e0824340e35f51cf7e7a8853a
hllee1021 <35593748+hllee1021@users.noreply.github.com>
2020-11-25 22:21:15 +0900

    working

--- controllers/estimator.py
+++ controllers/estimator.py
@@ -29,8 +29,45 @@ def get_estimator_task_info():
 #made_by 학림 evaluator_home에서 평가했을때 form action 라우터
 @controller.route("/estimator/evaluate", methods=["POST"])
 def evaluate():
-    '''estimator가 평가하는 라우터'''
+    '''학림's estimator가 평가하는 라우터'''
     score = request.form.get("score")
     p_np = request.form.get("p_np")
-    
+    if score>100 or score<0:
+        flash("점수는 0이상 100사이로 입력해야합니다")
+        return redirect("/")
+    else:
+        services.estimator.done_evaluation(,,score,p_np)
+        return redirect("/")
     return render_template("evaluator/evaluate_home.html")
+
+@controller.route("/task/download")
+def csv_file_download_with_stream():
+    """승수형이 submitter download한거 복사함 일단"""
+    odsf_type_id = int(request.args.get('odsf_type_id', 0))
+
+    if odsf_type_id != 0:
+        odsf_type = services.submitter.odsf_type_schema_info(odsf_type_id)
+    else:
+        return redirect("/my_task")
+
+    filename = f"{odsf_type['TaskName']}_{odsf_type['DataTypeName']}"
+
+    # schema에 맞는 df 생성
+    schema = json.loads(odsf_type['MappingInfo'])
+    schema = list(schema.keys())
+    temp_df = pd.DataFrame(columns=schema)
+
+    # dataframe을 저장할 IO stream 
+    output_stream = StringIO()
+
+    # 그 결과를 앞서 만든 IO stream에 저장
+    temp_df.to_csv(output_stream)
+    response = Response(
+        output_stream.getvalue(),
+        mimetype='text/csv',
+        content_type='application/octet-stream',
+    )
+
+    response.headers["Content-Disposition"] = f"attachment; filename={filename}.csv"
+
+    return response
