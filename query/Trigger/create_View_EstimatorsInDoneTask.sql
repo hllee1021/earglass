aeda72ab10f888bbe0458c45b735035dbea51ba8
Sebastian Jeong <sebastianrcnt@gmail.com>
2020-11-25 20:01:55 +0000

    [chor] submitter.py

--- query/Trigger/create_View_EstimatorsInDoneTask.sql
+++ query/Trigger/create_View_EstimatorsInDoneTask.sql
@@ -1,7 +1,7 @@
 -- estimators in done task
 
 CREATE VIEW EstimatorsInDoneTask AS
-    SELECT ROW_NUMBER() OVER() AS INDEX, T.TaskName, DISTINCT E.FK_idEstimator
+    SELECT ROW_NUMBER() OVER() AS IndexNum, T.TaskName, DISTINCT E.FK_idEstimator
     FROM TASK AS T, EVALUATION AS E, PARSING_DSF AS P
     WHERE T.Status = 'done'
     AND T.TaskName = P.TaskName
