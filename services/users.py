41db6c553b7d23b4a53a29c73976876d5fdc5ea5
Sebastian Jeong <sebastianrcnt@gmail.com>
2020-11-25 14:07:01 +0000

    [chor] change services.users.py user_id

--- services/users.py
+++ services/users.py
@@ -1,14 +1,16 @@
 from database.connection import *
 
+def get_user_by_index(user_index):
+    return queryone("SELECT * FROM USER WHERE idUSER=%s", (user_index,))
 
-def get_user_by_id(id):
-    # user 정보 받아오기
-    return queryone("SELECT * FROM USER WHERE idUSER=%s", (id,))
+def get_user_by_id(user_id):
+    '''user 정보 받아오기'''
+    return queryone("SELECT * FROM USER WHERE Id=%s", (user_id,))
 
 
-def verify_user(id, password):
-    # 유저 로그인 확인
-    user = get_user_by_id(id)
+def verify_user(user_id, password):
+    '''유저 로그인 확인'''
+    user = get_user_by_id(user_id)
     if not user:
         return False
     elif user["Password"] != password:
@@ -16,9 +18,9 @@ def verify_user(id, password):
     return True
 
 
-def sign_up(id, password, name, birth, phonenumber, gender, address, role):
+def sign_up(user_id, password, name, birth, phonenumber, gender, address, role):
     # 회원가입
-    return callproc('InsertNewUser', (id, password, name,
+    return callproc('InsertNewUser', (user_id, password, name,
                                       birth, phonenumber, gender, address, role))
 
 
